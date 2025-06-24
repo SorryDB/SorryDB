4a8cabe10d0f12cfe2b5273f2b240a6cc06cfe19
Paul-Lez <paul.lezeau@gmail.com>
2025-06-24 23:03:02 +0100

    Init: add sorry extractor

--- LeanUtils/LeanUtils.lean
+++ LeanUtils/LeanUtils.lean
@@ -1,3 +1,2 @@
--- This module serves as the root of the `LeanUtils` library.
--- Import modules here that should be built as part of the library.
-import LeanUtils.Basic
+import LeanUtils.ExtractSorry
+import LeanUtils.Test
