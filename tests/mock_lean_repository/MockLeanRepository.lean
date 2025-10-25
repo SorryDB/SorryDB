72cfa619b3ae9606593699be03be2d6f0a54deb4
Austin Letson <austin.letson@axiomatic-ai.com>
2025-10-25 09:05:05 -0400

    add files for lake build

--- tests/mock_lean_repository/MockLeanRepository.lean
+++ tests/mock_lean_repository/MockLeanRepository.lean
@@ -1,3 +1,7 @@
 -- This module serves as the root of the `MockLeanRepository` library.
 -- Import modules here that should be built as part of the library.
 import «MockLeanRepository».Basic
+import «MockLeanRepository».no_by
+import «MockLeanRepository».same_line
+import «MockLeanRepository».subdir.subdir
+import «MockLeanRepository».triple
--- tests/mock_lean_repository/lakefile.lean
+++ tests/mock_lean_repository/lakefile.lean
@@ -4,9 +4,9 @@ open Lake DSL
 package «mock_lean_repository» where
   -- add package configuration options here
 
+@[default_target]
 lean_lib «MockLeanRepository» where
   -- add library configuration options here
 
-@[default_target]
 lean_exe «mock_lean_repository» where
   root := `Main
