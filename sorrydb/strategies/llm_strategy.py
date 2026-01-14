88fc67990a81f9a580b5d6aa6be6d513e28572cd
Leopoldo <Leopoldo>
2026-01-14 19:33:55 +0100

    add qwen

--- sorrydb/strategies/llm_strategy.py
+++ sorrydb/strategies/llm_strategy.py
@@ -91,6 +91,12 @@ class LLMStrategy(SorryStrategy):
             self.model = ChatOpenAI(**model_config["params"])
         elif model_config["provider"] == "google":
             self.model = ChatGoogleGenerativeAI(**model_config["params"])
+        elif model_config["provider"] == "qwen":
+            self.model = ChatOpenAI(
+                api_key=getenv("OPENROUTER_API_KEY"),
+                base_url="https://openrouter.ai/api/v1",
+                model="qwen/qwen3-235b-a22b-thinking-2507",
+            )
         elif model_config["provider"] == "deepseek":
             use_api_provider = model_config.get("params", {}).get("api_provider", False)
             if use_api_provider:
